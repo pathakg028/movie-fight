30f27a88a3e09f5b5b5c8b66d1593d85bc38e58e
Gaurav Pathak <gauravpathak@192-168-1-8.tpgi.com.au>
2020-02-25 08:44:07 +1100

    handles error and DOM implementation

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const fetchData = async (searchTerm) => {
+const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '1098e545',
             s: searchTerm
         }
     });
-    return response.data.Search;
+    if(response.data.Error) {
+        return []
+    }
+        return response.data.Search;
+    
 };
 
 const input = document.querySelector('input')
@@ -16,8 +20,17 @@ const onInput = async event => {
         clearTimeout(timeOutId)
     }
     timeOutId = setTimeout( async () => {
-        const movie = await fetchData(event.target.value)
-        console.log(movie);
+        const movies = await fetchData(event.target.value)
+        // console.log(movie);
+        for(let movie of movies) {
+            const div = document.createElement('div')
+            div.innerHTML = `<img src="${movie.Poster}"/>
+                            <h1>${movie.Title}</h1>
+                         `
+
+        const target = document.querySelector('#target').appendChild(div)
+        }
+
 
     }, 500)
 }
