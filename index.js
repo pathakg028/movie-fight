7e2d07cbdcd1b61feb80cdcefe73fe9a0324a9b2
Gaurav Pathak <gauravpathak@192-168-1-8.tpgi.com.au>
2020-02-25 10:13:16 +1100

    added click  eventListener to open and close open list

--- index.js
+++ index.js
@@ -33,12 +33,18 @@ const onInput = async event => {
     }
     timeOutId = setTimeout( async () => {
         const movies = await fetchData(event.target.value)
+        if(!movies.length) {
+            dropdown.classList.remove('is-active');
+            return;
+        }
+        resultsWrapper.innerHTML='';
         dropdown.classList.add('is-active')
         // console.log(movie);
         for(let movie of movies) {
             const option = document.createElement('a')
+            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
             option.classList.add('dropdown-item');
-            option.innerHTML = `<img src="${movie.Poster}"/>
+            option.innerHTML = `<img src="${imgSrc}"/>
                             ${movie.Title}
                          `
 
@@ -50,3 +56,9 @@ const onInput = async event => {
 }
 
 input.addEventListener('input', onInput)
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)) {
+        dropdown.classList.remove('is-active')
+    }
+})
