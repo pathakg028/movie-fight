62ec5f5618165bbabdb30dd4146c301d572df0b1
Gaurav Pathak <gauravpathak@192-168-1-8.tpgi.com.au>
2020-02-25 09:28:37 +1100

    added dynamic style

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ const fetchData = async searchTerm => {
     
 };
 
-const input = document.querySelector('input')
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results')
 
 let timeOutId
 const onInput = async event => {
@@ -21,14 +33,16 @@ const onInput = async event => {
     }
     timeOutId = setTimeout( async () => {
         const movies = await fetchData(event.target.value)
+        dropdown.classList.add('is-active')
         // console.log(movie);
         for(let movie of movies) {
-            const div = document.createElement('div')
-            div.innerHTML = `<img src="${movie.Poster}"/>
-                            <h1>${movie.Title}</h1>
+            const option = document.createElement('a')
+            option.classList.add('dropdown-item');
+            option.innerHTML = `<img src="${movie.Poster}"/>
+                            ${movie.Title}
                          `
 
-        const target = document.querySelector('#target').appendChild(div)
+        resultsWrapper.appendChild(option)
         }
 
 
