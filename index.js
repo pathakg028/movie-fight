18104e5c84164934346cee79b70a9b07c1184891
Gaurav Pathak <gauravpathak@192-168-1-8.tpgi.com.au>
2020-02-25 04:50:55 +1100

    network connection established

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '1098e545',
+            s: searchTerm
+        }
+    });
+    return response.data.Search;
+};
+
+const input = document.querySelector('input')
+
+const inputMovie = async (event) => {
+    const searchedMovie = await fetchData(event.target.value)
+    console.log(searchedMovie);
+}
+
+input.addEventListener('input', inputMovie)
